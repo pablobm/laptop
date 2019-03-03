ee21d5b841de04cf8163236e8275757391655a3c
Pablo Brasero <pablo@pablobm.com>
2019-03-03 16:21:16 +0000

    My Git config

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+LAPTOP_RUNNER=$(readlink -f "$0")
+LAPTOP_DIR=$(dirname "$LAPTOP_RUNNER")
+
 if ! command -v sudo >/dev/null; then
   echo 'FATAL: Please install sudo before running this script.'
   exit 1
@@ -20,3 +23,11 @@ sudo apt install -y curl
 # User by GnuPG to display pictures linked to keys.
 # Recommended but not required.
 sudo apt install -y xloadimage
+
+GITCONFIG="$HOME/.gitconfig"
+if [ -f "$GITCONFIG" ] || [ -L "$GITCONFIG" ]; then
+  echo "FATAL: The file $GITCONFIG already exists. Stopping to avoid overwriting your settings."
+  echo "TIP: \`rm '$GITCONFIG'\` if you are happy to replace it."
+  exit 1
+fi
+ln -s "$LAPTOP_DIR/configs/dot.gitconfig" "$GITCONFIG"
