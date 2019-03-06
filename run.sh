661717319ca993729b9911609809b02660473305
Pablo Brasero <pablo@pablobm.com>
2019-03-06 20:08:52 +0000

    Neovim defaults to tabs, so I'll go with that

--- run.sh
+++ run.sh
@@ -4,14 +4,14 @@ LAPTOP_RUNNER=$(readlink -f "$0")
 LAPTOP_DIR=$(dirname "$LAPTOP_RUNNER")
 
 if ! command -v sudo >/dev/null; then
-  echo 'FATAL: Please install sudo before running this script.'
-  exit 1
+	echo 'FATAL: Please install sudo before running this script.'
+	exit 1
 fi
 
 if ! sudo -v 2>/dev/null; then
-  echo 'FATAL: Please enable the current user to use sudo.'
-  echo "TIP: Try with \`usermod -a -G sudo $(whoami)\` as root. Then you'll have to log out and back in or somethink like that..."
-  exit 1
+	echo 'FATAL: Please enable the current user to use sudo.'
+	echo "TIP: Try with \`usermod -a -G sudo $(whoami)\` as root. Then you'll have to log out and back in or somethink like that..."
+	exit 1
 fi
 
 sudo apt update
@@ -34,8 +34,8 @@ sudo apt install -y dirmngr
 
 GITCONFIG="$HOME/.gitconfig"
 if [ -f "$GITCONFIG" ] || [ -L "$GITCONFIG" ]; then
-  echo "FATAL: The file $GITCONFIG already exists. Stopping to avoid overwriting your settings."
-  echo "TIP: \`rm '$GITCONFIG'\` if you are happy to replace it."
-  exit 1
+	echo "FATAL: The file $GITCONFIG already exists. Stopping to avoid overwriting your settings."
+	echo "TIP: \`rm '$GITCONFIG'\` if you are happy to replace it."
+	exit 1
 fi
 ln -s "$LAPTOP_DIR/configs/dot.gitconfig" "$GITCONFIG"
