c56683fb244091e86a731144fe9928aa5fee4892
Pablo Brasero <pablo@pablobm.com>
2025-05-30 09:19:28 +0100

    Minimal editor config

--- configs/dot.config/nvim/init.lua
+++ configs/dot.config/nvim/init.lua
@@ -1,1 +1,11 @@
+-- Show line numbers
+vim.o.nu = true
+
+-- Indent with 2 spaces
+vim.o.smartindent = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+
+-- lazy.vim
 require("config.lazy")
