fe1d677fade9b9913e8b98d0afc08fb0915aa34d
Pablo Brasero <pablo@pablobm.com>
2025-06-28 22:41:01 +0100

    Neovim: use lspconfig to simplify configuration

--- configs/dot.config/nvim/lua/config/lsp.lua
+++ configs/dot.config/nvim/lua/config/lsp.lua
@@ -1,56 +1,7 @@
-function EnableStandardRb()
-  vim.lsp.config(
-    'standardrb',
-    {
-      cmd = {'bundle', 'exec', 'standardrb', '--lsp'},  
-      root_dir = vim.fs.dirname(
-        vim.fs.find(
-          {'.git', 'Gemfile'}, 
-          { upward = true }
-        )[1]
-      ),
-      filetypes = { 'ruby' }
-    }
-  )
-  vim.lsp.enable('standardrb')
-end
-
-vim.api.nvim_create_user_command('StandardRb', EnableStandardRb, {})
-
-function EnableRubocop()
-  vim.lsp.config(
-    'rubocop',
-    {
-      cmd = {'bundle', 'exec', 'rubocop', '--lsp'},  
-      root_dir = vim.fs.dirname(
-        vim.fs.find(
-          {'.git', 'Gemfile', '.rubocop.yml'}, 
-          { upward = true }
-        )[1]
-      ),
-      filetypes = { 'ruby' }
-    }
-  )
-  vim.lsp.enable('rubocop')
-end
-
-vim.api.nvim_create_user_command('Rubocop', EnableRubocop, {})
-
-function EnableElixirLs()
-  vim.lsp.config(
-    'elixir-ls',
-    {
-      cmd = { vim.fs.joinpath(vim.fn.stdpath("data"), "mason", "bin", "elixir-ls") },
-      root_dir = vim.fs.dirname(
-        vim.fs.find(
-          {'.git', 'mix.exs'}, 
-          { upward = true }
-        )[1]
-      ),
-      filetypes = { 'elixir' }
-    }
-  )
-  vim.lsp.enable('elixir-ls')
-end
-
-vim.api.nvim_create_user_command('ElixirLs', EnableElixirLs, {})
+vim.api.nvim_create_user_command(
+  "EnableLsp",
+  function(opts)
+    vim.lsp.enable(opts.args)
+  end,
+  { nargs = 1 }
+)
