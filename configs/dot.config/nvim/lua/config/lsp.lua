72b1b352bf89ad257750485f4449bb7ddbf5b75f
Pablo Brasero <pablo@pablobm.com>
2025-06-29 22:50:49 +0100

    Neovim: command to format current file

--- configs/dot.config/nvim/lua/config/lsp.lua
+++ configs/dot.config/nvim/lua/config/lsp.lua
@@ -1,7 +1,17 @@
+-- Manual enable of LSPs
 vim.api.nvim_create_user_command(
   "EnableLsp",
   function(opts)
     vim.lsp.enable(opts.args)
   end,
   { nargs = 1 }
+)
+
+-- Format file
+vim.api.nvim_create_user_command(
+  "Fix",
+  function()
+    vim.lsp.buf.format()
+  end,
+  {}
 )
