1d37261d09c7219c7c8b9f7c8e6829614a6a5e29
Pablo Brasero <pablo@pablobm.com>
2025-06-29 23:12:12 +0100

    Neovim: show LSP diagnostics

--- configs/dot.config/nvim/lua/config/lsp.lua
+++ configs/dot.config/nvim/lua/config/lsp.lua
@@ -1,5 +1,21 @@
+--
+-- With help from https://rsdlt.github.io/posts/rust-nvim-ide-guide-walkthrough-development-debug/
+--
+
 -- Always show LSP status column, to avoid expand-collapse
 vim.o.signcolumn = "yes"
+
+-- updatetime: set updatetime for CursorHold
+-- vim.api.nvim_set_option("updatetime", 300) 
+
+vim.api.nvim_create_autocmd(
+  "CursorHold", {
+    pattern = "*",
+    callback = function()
+      vim.diagnostic.open_float(nil, { focusable = false })
+    end,
+  }
+)
 
 -- Manual enable of LSPs
 vim.api.nvim_create_user_command(
