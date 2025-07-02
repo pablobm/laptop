8841365082902a1d8fc07fc3b4619cc8a9a86015
Pablo Brasero <pablo@pablobm.com>
2025-07-02 16:17:43 +0100

    Neovim: share clipboard with system

--- configs/dot.config/nvim/init.lua
+++ configs/dot.config/nvim/init.lua
@@ -7,6 +7,10 @@ vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.expandtab = true
 
+-- Use the system clipboard.
+-- Taken from Julia Evans's "The Secrets Rules of the Terminal"
+vim.o.clipboard = "unnamed"
+
 -- lazy.vim
 require("config.lazy")
 require("config.lsp")
