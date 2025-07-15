a8369eae1ac44e764accc94e9e3b24526f87edaf
Pablo Brasero <pablo@pablobm.com>
2025-07-15 14:50:49 +0100

    Fish: machine-dependent paths not to be Universal

--- configs/dot.config/fish/config.fish
+++ configs/dot.config/fish/config.fish
@@ -1,3 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+switch (uname)
+  case Darwin
+    set -p PATH /opt/homebrew/bin
+end
+set -p PATH "$HOME/bin"
