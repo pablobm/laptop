f15573b995b30e076cf5c160ddf57f40f4111d19
Pablo Brasero <pablo@pablobm.com>
2025-12-24 00:01:59 +0100

    Neovim: trying to make sense of the new Treesitter config

--- configs/dot.config/nvim/lua/plugins/treesitter.lua
+++ configs/dot.config/nvim/lua/plugins/treesitter.lua
@@ -2,14 +2,11 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
-    require("nvim-treesitter.configs").setup {
-      ensure_installed = {
-        "elixir", "eex", "heex",
-        "lua",
-        "ruby",
-        "rust",
-      },
-      highlight = { enable = true, }
-    }
+    require("nvim-treesitter").install({
+      "elixir", "eex", "heex",
+      "lua",
+      "ruby",
+      "rust",
+    })
   end
 }
