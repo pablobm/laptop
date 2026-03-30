947015b91461a525dcdbf940c73ef241aa7d75d2
Pablo Brasero <pablo@pablobm.com>
2026-03-30 10:29:15 +0100

    Nvim: more robust code for LSP config

--- configs/dot.config/nvim/lua/config/lsp.lua
+++ configs/dot.config/nvim/lua/config/lsp.lua
@@ -32,17 +32,17 @@ vim.api.nvim_create_user_command(
   {}
 )
 
-function rubocop_yml_expected(expecting_rubocop_yml)
+local function rubocop_yml_expected(expecting_rubocop_yml)
   return function(bufnr, on_dir)
     -- Get the name of the current buffer. This will be a full path if a file
-    file_path = vim.fn.bufname(bufnr)
+    local file_path = vim.fn.bufname(bufnr)
 
     -- Traverse directories up the hierarchy until one of Gemfile or .git is found.
     -- This should be the root of the project.
-    _root_dir = vim.fs.root(file_path, {"Gemfile", ".git"})
+    local _root_dir = vim.fs.root(file_path, {"Gemfile", ".git"})
 
     -- Is there a .rubocop.yml at the root?
-    _rubocop_yml_present = not not (_root_dir and vim.uv.fs_stat(_root_dir .. "/.rubocop.yml"))
+    local _rubocop_yml_present = not not (_root_dir and vim.uv.fs_stat(_root_dir .. "/.rubocop.yml"))
 
     -- Assuming that a _root_dir was found
     -- If there is a .rubocop.yml AND we expected it to be present, call on_dir().
